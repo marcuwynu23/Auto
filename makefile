e5397b70817f1b8a750650d36f82525ebb703ce6
marcuwynu23 <marcuwynu23@gmail.com>
2023-01-25 22:50:30 +0800

    feat: added execution type ASYNC and SYNC.

--- makefile
+++ makefile
@@ -1,14 +1,25 @@
 #constants variable
 BIN=bin/auto.exe
+
+
+# icon resource 
+ICO=resources/icon.rc
+RES=resources/resource.res
+
 SRC=$(wildcard src/*.cpp)
 
-# compiling source file
-all:
-	c++ -std=c++11 -o $(BIN)  -I include $(SRC) 
 
+# compiling source file
+all: compile_icon compile_source
 # running executable
 run:
 	$(BIN)
+
+compile_source:
+	c++ -std=c++11 $(RES) -o $(BIN)  -I include $(SRC) 
+
+compile_icon:
+	windres $(ICO) -O coff -o $(RES)
 
 
 # cleaning executable
--- src/lib.cpp
+++ src/lib.cpp
@@ -1,3 +1,7 @@
+/*
+ *   Copyright (c) 2023 
+ *   All rights reserved.
+ */
 #include "lib.h"
 #include <direct.h>
 #include <fstream>
@@ -8,7 +12,8 @@
 #include <vector>
 
 // user functions
-void __termExecute(std::string command) {
+//function to execute the commands Concurrently
+void __termExecuteSync(std::string command) {
   std::mutex m;
   std::thread t{[&] {
     std::lock_guard<std::mutex> lock{m};
@@ -16,34 +21,84 @@ void __termExecute(std::string command) {
   }};
   t.join();
 }
-void __termPrint(std::string str) {
+
+
+
+//function to print the command in terminal in Parallel
+void __termPrintSync(int type,std::string str) {
   std::mutex m;
   std::thread t{[&] {
     std::lock_guard<std::mutex> lock{m};
-    cout << str.c_str() << endl;
+    cout <<"  type: " << type <<" : " << str.c_str() << endl;
   }};
   t.join();
 }
 
+
+
+// function for executing command in Parallel
+void __termExecuteAsync(std::string command) {
+ system(command.c_str());
+}
+
+
+
+//function for printing the command in terminal in Parallel
+void __termPrintAsync(int type,std::string str) {
+    cout <<"  type: "<< type << " : "<< str.c_str() << endl;  
+}
+
+
+
+// function to read String in A Line
 void __readStringLine(std::string fileName) {
   std::fstream newfile;
-
   newfile.open(fileName, std::ios::in);
-
+  int lineNumber = 1;
+  int executionType = 1;
   if (newfile.is_open()) {
     string tp;
     cout << "Autofile: " << endl;
     cout << " Start:\n " << endl;
     while (getline(newfile, tp)) {
-
-      
-      if (tp.length() > 0) {
-        if (tp[0] != '#' && tp[0] == '-') {
-          __termPrint(tp);
+      if(lineNumber == 1){
+        //check if first character of line 1 is contains : 
+        if( tp.substr(0,2) == "::"){
+           //remove : from the first line
           tp = tp.erase(0, 1);
-          __termExecute("start cmd /K " + tp);
+          cout << "  type: "+tp << endl;
+          if(tp == "SYNC"){
+            executionType = 0;
+          }else if(tp == "ASYNC"){
+            executionType = 1;
+          }
         }
       }
+      //check if the autofile content length is not empty
+      if (tp.length() > 0) {
+        //if is not empty, check the first character in every line if
+        // not contains # because this is for commenting and
+        // it is contains - which is the first character of a executable command
+        // to be execute.
+        if (tp[0] != '#' && tp[0] == '-') {
+          if(executionType == 1){
+          //print the commands
+          __termPrintAsync(executionType,tp);
+          //remove the first character
+          tp = tp.erase(0, 1);
+          //execute the command with start -
+          __termExecuteAsync("start \"" + tp + "\" cmd /K " + tp);
+          }else if(executionType == 0){
+               //print the commands
+          __termPrintSync(executionType,tp);
+          //remove the first character
+          tp = tp.erase(0, 1);
+          //execute the command with start -
+          __termExecuteSync("start \"" + tp + "\" cmd /K " + tp);
+          }
+        }
+      }
+      lineNumber++;
     }
     cout << "\n End:\n " << endl;
     newfile.close();
