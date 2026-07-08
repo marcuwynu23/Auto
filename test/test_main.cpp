dcdce46463b56ac821cd1e56e0fbd96400c3d08e
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2026-07-08 12:53:00 +0800

    docs: update copyright information and license details in test_main.cpp

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,7 +1,12 @@
 /*
- *   Copyright (c) 2023
- *   All rights reserved.
- * 
+ *   Copyright (c) 2023 Mark Wayne Menorca
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
  *   Test suite for Auto library
  */
 
